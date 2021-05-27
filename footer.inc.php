375cf8925165a5ea787137adfbbe585651d5b945
Giperact <84937249+Giperact@users.noreply.github.com>
2021-05-27 23:05:50 +0300

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,3 @@
+<div class="footer">
+        <h2>We have studied PHP basis!</h2>
+</div>
---
+++ index.php
@@ -0,0 +1,17 @@
+
+
+<?php 
+ $p = 'Greetings on my page!';
+?>
+
+<?php 
+ $name = 'Ivan';
+ $surname = 'Kurbatov';
+ $city = 'Moscow';
+ $age = 39;
+?>
+
+
+<?php
+include 'main.php';
+?>
